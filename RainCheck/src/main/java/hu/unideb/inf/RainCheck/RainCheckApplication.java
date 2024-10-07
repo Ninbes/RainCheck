558ecae1b60a2992793803875be7405d826884c3
Bence <pbence.mate@gmail.com>
2024-10-07 16:30:37 +0200

    Initialize

---
+++ RainCheck/src/main/java/hu/unideb/inf/RainCheck/RainCheckApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.RainCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainCheckApplication.class, args);
+	}
+
+}
