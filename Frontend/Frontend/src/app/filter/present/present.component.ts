cd87566782eca224369b4ee6214cc14600839181
2003siposzoltan <2003siposzoltan@gmail.com>
2024-10-30 12:20:31 +0100

    Http request for data.json

--- Frontend/Frontend/src/app/filter/present/present.component.ts
+++ Frontend/Frontend/src/app/filter/present/present.component.ts
@@ -2,6 +2,14 @@ import { Component, inject, Input, input, OnInit, signal } from '@angular/core';
 import { fakeData } from '../../fakeData';
 import { DatePipe } from '@angular/common';
 import { FilterComponent } from '../filter.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface CityData {
+  id: string;
+  city: string;
+  temperature: string;
+}
 
 @Component({
   selector: 'app-present',
@@ -24,14 +32,20 @@ export class PresentComponent{
   }
 
   updateTemperature(): void {
-    const cityData = this.getTemperatureData().find(item => item.city.toLowerCase() === this.city.toLowerCase());
-    this.temperature = cityData ? cityData.temperature : '??';
+    /*const cityData = this.getTemperatureData().find(item => item.city.toLowerCase() === this.city.toLowerCase());
+    this.temperature = cityData ? cityData.temperature : '??';*/
+    this.getTemperatureData().subscribe(data => {
+      const cityData = data.find((item: CityData) => item.city.toLowerCase() === this.city.toLowerCase());
+      this.temperature = cityData ? cityData.temperature : '??';
+    });
   }
 
+  constructor(private http: HttpClient) {}
   getTemperatureData() {
-    return [
+    /*return [
       { id: '1', city: 'Budapest', temperature: '16' },
       { id: '2', city: 'Debrecen', temperature: '12' }
-    ];
+    ];*/
+    return this.http.get<CityData[]>('http://localhost:3000/data');
   }
 }
