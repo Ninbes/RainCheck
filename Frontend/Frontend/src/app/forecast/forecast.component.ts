22b75cfe0a04bc7c6c2f10d03390dbc2ab7d6fdb
2003siposzoltan <2003siposzoltan@gmail.com>
2024-11-13 15:24:23 +0100

    Create one day forecast cards.

--- Frontend/Frontend/src/app/forecast/forecast.component.ts
+++ Frontend/Frontend/src/app/forecast/forecast.component.ts
@@ -1,7 +1,7 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit} from '@angular/core';
 import { Day, Forecast, Hour } from './forecast.model';
 import { ForecastService } from '../forecast.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-forecast',
@@ -12,17 +12,21 @@ import { CommonModule } from '@angular/common';
 })
 export class ForecastComponent implements OnInit{
 forecast!: Forecast;
-day!: Day;
+days!: Day[];
+
 hours!: Hour[];
 
   constructor(private service: ForecastService) {
     
   }
   ngOnInit(): void {
-    this.service.findForecast("budapest").subscribe({
+    this.service.findForecast("debrecen").subscribe({
       next: value=>{
         this.forecast=value;
+        //this.hours=this.day.hours;
+        this.days=this.forecast.days;
       }
     })
   }
+  
 }
