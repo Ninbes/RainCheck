307bcf57dbec45c72f418e60b7eb82b659754b7c
Papp Bence Mate <pbence.mate@gmail.com>
2024-11-24 22:03:46 +0100

    Api handling

--- RainCheck/src/main/java/hu/unideb/inf/RainCheck/service/WeatherService.java
+++ RainCheck/src/main/java/hu/unideb/inf/RainCheck/service/WeatherService.java
@@ -1,68 +1,71 @@
 package hu.unideb.inf.RainCheck.service;
 
+import hu.unideb.inf.RainCheck.config.VisualCrossingConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import utils.DateUtil;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class WeatherService {
     private final RestTemplate restTemplate;
     private final ApiCallCounterService apiCallCounter;
+    private final VisualCrossingConfig visualCrossingConfig;
 
-    @Value("${weather.api.key}")
-    private String apiKey;
-
-    public WeatherService(RestTemplate restTemplate, ApiCallCounterService apiCallCounter) {
+    public WeatherService(RestTemplate restTemplate, ApiCallCounterService apiCallCounter, VisualCrossingConfig visualCrossingConfig) {
         this.restTemplate = restTemplate;
         this.apiCallCounter = apiCallCounter;
+        this.visualCrossingConfig = visualCrossingConfig;
     }
 
-    public String getWeatherDataForOneDay(String location) {
+    public String getWeatherDataForOneDay(String location) throws URISyntaxException {
         String today = DateUtil.getToday();
-        String url = UriComponentsBuilder.fromHttpUrl("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+        String url = UriComponentsBuilder.fromHttpUrl(visualCrossingConfig.getBaseUrl()
                         + location + "/" + today + "/" + today)
                 .queryParam("unitGroup", "metric")
                 .queryParam("elements", "datetime,tempmax,tempmin,temp,precip,preciptype,precipcover,windspeedmax")
                 .queryParam("include", "hours")
-                .queryParam("key", apiKey)
+                .queryParam("key", visualCrossingConfig.getKey())
                 .queryParam("contentType", "json")
                 .toUriString();
         apiCallCounter.incrementAndGet();
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject(new URI(url), String.class);
     }
-    public String getWeatherDataForOneWeek(String location) {
+    public String getWeatherDataForOneWeek(String location) throws URISyntaxException {
         String today = DateUtil.getToday();
         String oneWeek = DateUtil.getSevenDaysFromToday();
 
-        String url = UriComponentsBuilder.fromHttpUrl("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+        String url = UriComponentsBuilder.fromHttpUrl(visualCrossingConfig.getBaseUrl()
                         + location + "/" + today + "/" + oneWeek)
                 .queryParam("unitGroup", "metric")
                 .queryParam("elements", "datetime,tempmax,tempmin,precip,preciptype,windspeedmax")
                 .queryParam("include", "days")
-                .queryParam("key", apiKey)
+                .queryParam("key", visualCrossingConfig.getKey())
                 .queryParam("contentType", "json")
                 .toUriString();
 
-        apiCallCounter.incrementAndGet();
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject(new URI(url), String.class);
     }
-    public String getWeatherDataForTwoWeek(String location) {
+    public String getWeatherDataForTwoWeek(String location) throws URISyntaxException {
         String today = DateUtil.getToday();
         String twoWeek = DateUtil.getFourteenDaysFromToday();
 
-        String url = UriComponentsBuilder.fromHttpUrl("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+        String url = UriComponentsBuilder.fromHttpUrl(visualCrossingConfig.getBaseUrl()
                         + location + "/" + today + "/" + twoWeek)
                 .queryParam("unitGroup", "metric")
                 .queryParam("elements", "datetime,tempmax,tempmin,precip,preciptype,windspeedmax")
                 .queryParam("include", "days")
-                .queryParam("key", apiKey)
+                .queryParam("key", visualCrossingConfig.getKey())
                 .queryParam("contentType", "json")
                 .toUriString();
 
-        apiCallCounter.incrementAndGet();
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject(new URI(url), String.class);
     }
 }
 
